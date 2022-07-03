1c501c7d18dab30c3f3e373a922fce02c54f976f
ShareefRahat <imr4h47@gmail.com>
2022-07-03 10:19:15 +0600

    auth guard added in app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller('app')
+@Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get('hello')
-  getHello(): string {
-    return this.appService.getHello();
-  }
-  @Get('bye')
-  sayBye(): string {
-    return this.appService.sayBye();
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
